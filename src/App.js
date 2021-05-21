85720eb3480b29e3d2c67629b1f83f4be8a910af
Mohammad Faisal <faisalhmohd@gmail.com>
2021-05-21 18:52:32 +0600

    Add initial files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Button, Container, Row, Col, InputGroup, FormControl} from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 function App() {
+  const [age, setAge] = useState(2);
+  const [grams, setGrams] = useState(100);
+
+  const gramsToCalculate = grams < 20 ? grams : 20;
+  const lifeSpan = parseInt(age) + parseInt(gramsToCalculate);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col xs lg="6">
+            <h5>DEMO</h5>
+            <h1>cloomis</h1>
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+                <InputGroup.Text id="basic-addon1">I am</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                value={age}
+                aria-label="age"
+                aria-describedby="basic-addon1"
+                onChange={e => setAge(e.target.value)}
+              />
+              <InputGroup.Append>
+                <InputGroup.Text id="basic-addon2">years old and </InputGroup.Text>
+              </InputGroup.Append>
+            </InputGroup>
+            <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+                <InputGroup.Text id="basic-addon1">I eat</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                value={grams}
+                aria-label="grams"
+                aria-describedby="basic-addon1"
+                onChange={e => setGrams(e.target.value)}
+              />
+              <InputGroup.Append>
+                <InputGroup.Text id="basic-addon2">grams of nuts per day.</InputGroup.Text>
+              </InputGroup.Append>
+            </InputGroup>
+            <h4 className="result">My expected lifespan is <span>{lifeSpan}</span> years!</h4>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
